73fed1a210d6ed069f6b884314db80cc52636e8c
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-12-04 21:32:35 -0800

    in budget.js got transaction description and amount to add and remove from budget list and got balance to increase accordingly upon add (but not deduct upon remove)

--- budget.js
+++ budget.js
@@ -1,26 +1,48 @@
-const transactionsList = document.getElementById('budget-list');
+const transList = document.getElementById('budget-list');
 
-transactionsList.addEventListener('click',(e) => {
+transList.addEventListener('click',(e) => {
     if (e.target.className === 'remove') {
         const lineItem = e.target.parentElement;
         lineItem.remove();
+        //Deduct $amount from balance
     }
 });
 
-const addTransaction = function(e) {
-    e.preventDefault();
-    const input = document.getElementById('transaction');
-    // console.log(input);
-    const inputText = input.value;
-    const span = document.createElement('span');
-    span.textContent = inputText;
-    const li = document.createElement('li');
-    li.appendChild(span);
-    transactionsList.appendChild(li);
+let balanceTotal = 0;
+function updateBalance(amount) {
+    balanceTotal += amount;
 }
 
-//The below code works, but baybe add the eventlistener to parent element instead
+const addTrans = function(e) {
+    e.preventDefault();
+    
+    const inputTrans = document.getElementById('transaction');
+    const inputTransText = inputTrans.value;
+    const spanTrans = document.createElement('span');
+    spanTrans.textContent = `${inputTransText}: `;
+
+    const inputAmt = document.getElementById('amount');
+    const inputAmtText = Number(inputAmt.value);
+    const spanAmt = document.createElement('span');
+    spanAmt.textContent = `$${inputAmtText} `;
+
+    const li = document.createElement('li');
+    li.appendChild(spanTrans);
+    li.appendChild(spanAmt);
+    transList.appendChild(li);
+    const newRemoveBtn = document.createElement('a');
+    newRemoveBtn.textContent = 'Remove';
+    newRemoveBtn.className = 'remove';
+    li.appendChild(newRemoveBtn);
+
+    updateBalance(inputAmtText);
+    const balanceHead = document.getElementById('balance');
+    balanceHead.textContent = `Balance: $${balanceTotal}`;
+
+}
+
+//The below code works, but maybe add the eventlistener to parent element instead
 const addBtn = document.querySelector('a.add-item');
 addBtn.addEventListener('click', (e)=> {
-    addTransaction(e);
+    addTrans(e);
 });
